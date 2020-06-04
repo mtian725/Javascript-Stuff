652e7a657a608189281537da4416e521bca124b5
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-04 16:35:17 -0400

    guass

---
+++ app.js
@@ -0,0 +1,63 @@
+// This file will hold all the functions
+// Functions are a part of CMSC330 FALL2019 Project 7 part 1 and 2
+
+const guass = num => {
+  if (num < 0) {
+    return -1;
+  }
+  else {
+    let sum = 0;
+    while (num > 0) {
+      sum += num;
+      num--;
+    }
+    return sum;
+  }
+};
+
+const in_range = () => {
+  
+};
+
+const subset = () => {
+
+};
+
+const mean = () => {
+
+};
+
+const to_decimal = () => {
+
+};
+
+const factorize = () => {
+
+};
+
+const rotate = () => {
+
+};
+
+const substr = () => {
+
+};
+
+const longest_sequence = () => {
+
+};
+
+const enqueue = () => {
+
+};
+
+const dequeue = () => {
+
+};
+
+const peek = () => {
+
+};
+
+export { guass, in_range, subset, mean, to_decimal, factorize, rotate, substr,
+  longest_sequence, enqueue, dequeue, peek };
---
+++ test.js
@@ -0,0 +1,10 @@
+
+import { guass, in_range, subset, mean, to_decimal, factorize, rotate, substr,
+  longest_sequence, enqueue, dequeue, peek } from './app.js';
+
+// Test guass
+console.log('Testing guass');
+console.log(
+  guass(0) === 0, guass(3) === 6, guass(10) === 55, guass(-17) === -1
+);
+console.log();
